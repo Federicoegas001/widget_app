1f9d6a5813af0e0e7193b70ad69954c0e62f3145
Federicoegas001 <egasagustin@gmail.com>
2024-06-06 11:55:20 -0300

    clase Menu Item y Lista de MenuItems

---
+++ lib/config/menu/menu_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem(
+      {required this.title,
+      required this.subtitle,
+      required this.link,
+      required this.icon});
+}
+
+const List<MenuItem> appMenuItems = [
+  MenuItem(
+    title: 'botones',
+    subtitle: 'varios botones',
+    link: '/botones',
+    icon: Icons.smart_button_outlined,
+  ),
+  MenuItem(
+    title: 'tarjetas',
+    subtitle: 'varios tarjetas',
+    link: '/tarjetas',
+    icon: Icons.credit_card,
+  )
+];
